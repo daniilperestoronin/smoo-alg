50319da9d1403acad9a9697912b914c8f300bb36
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2018-03-03 20:54:02 +0300

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+# smoo-alg
+Algorithm for solving stochastic multi-objective optimization problem
+
+## Authors
+* **[Daniil Perestoronin](https://github.com/daniilperestoronin/)** 
+
+## License
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
---
+++ algorithm.py
@@ -0,0 +1,20 @@
+"""
+Algorithm for solving stochastic multi-objective optimization problem
+
+author Perestoronin Daniil
+"""
+
+
+def print_matrix(m):
+    for row in m:
+        for el in row:
+            print(el, end=' ')
+        print()
+
+
+a = [
+    [1, 2, 3],
+    [4, 5, 6]
+]
+
+print_matrix(a)
